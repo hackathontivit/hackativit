8bfefdee3cf28032aabf2d3cc29e4263f13469bf
Carolina Rodrigues <laboratoriaero@gmail.com>
2018-11-08 11:18:11 -0200

    Implementações de Bootstrap, JQuery e Firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAmsYfHFv1SbO9BYZWLNDU9CiyTOBYgh30",
+  authDomain: "hackathon-tivit.firebaseapp.com",
+  databaseURL: "https://hackathon-tivit.firebaseio.com",
+  projectId: "hackathon-tivit",
+  storageBucket: "hackathon-tivit.appspot.com",
+  messagingSenderId: "973413685809"
+};
+firebase.initializeApp(config);
