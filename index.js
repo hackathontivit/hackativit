867b802d3fe5da6d4d0aaf7a954e8eb7e32a3f57
Carolina Rodrigues <laboratoriaero@gmail.com>
2018-11-08 17:11:29 -0200

    Cadastro e Login empresarial, funcionando

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-var database = firebase.database();
+$(document).ready(function () {
+  //////////// FIRE BASE "CADASTRO EMPRESARIAL"
+  $(".sign-up-company").click(function(event){
+    event.preventDefault();
+
+    var email = $(".su-email-address-company").val();
+    var password = $(".su-password-company").val();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(){
+
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+  });
+
+
+  //////////// FIRE BASE "ENTRAR EMPRESARIAL"
+  $(".sign-in-company").click(function(event){
+    event.preventDefault();
+
+    var email = $(".si-email-address-company").val();
+    var password = $(".si-password-company").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(){
+
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+  });
+
+});
+
